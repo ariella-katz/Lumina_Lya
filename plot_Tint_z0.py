9b91f947d9e51a8530be8b7ff8b231358fec3fd4
Ariella Katz <dal440490@juno-l-01.localdomain>
2026-07-13 16:36:10 -0500

    switched plotting back to banded

--- plot_Tint_z0.py
+++ plot_Tint_z0.py
@@ -21,11 +21,11 @@ def transmission_integrated_z0_old(s):
     T_int_ultrared = np.sum(transmission[:,:,i4:]*(4000./800.)/1500., axis=2)
     return z0, T_int_ultrablue, T_int_blue, T_int_center, T_int_red, T_int_ultrared
 
-def transmission_integrated_z0_banded(z0_ss):
+def transmission_integrated_z0(z0_ss):
     "Takes in a file created by calculate_tau.py, which includes z0, taus, transmission, Dvs"
-    n_chunks = len(z0_ss)
+    n_chunks = int(np.sqrt(len(z0_ss)))
     s0 = z0_ss[0]
-    z0 = s0.attrs['Redshift']
+    z0 = float(np.asarray(s0.attrs['Redshift']).squeeze())
     tau_band_avgs_0 = s0['tau_band_avgs'][:]
     chunk_size = tau_band_avgs_0.shape[1]
     T_int_ultrablue = np.zeros((n_chunks*chunk_size, n_chunks*chunk_size))
@@ -42,12 +42,12 @@ def transmission_integrated_z0_banded(z0_ss):
     T_int_center[x1:x1+chunk_size,y1:y1+chunk_size] = np.exp(-tau_band_avgs_0[2])
     T_int_red[x1:x1+chunk_size,y1:y1+chunk_size] = np.exp(-tau_band_avgs_0[3])
     T_int_ultrared[x1:x1+chunk_size,y1:y1+chunk_size] = np.exp(-tau_band_avgs_0[4])
-    for chunk in range(n_chunks-1)+1:
+    for chunk in range(1, n_chunks):
         s_chunk = z0_ss[chunk]
         chunk_num = s_chunk.attrs['Chunk']
         tau_band_avgs_chunk = s_chunk['tau_band_avgs'][:]
-        x1 = chunk_size * (chunk_num % int(np.sqrt(n_chunks)))
-        y1 = chunk_size * (chunk_num // int(np.sqrt(n_chunks)))
+        x1 = chunk_size * (chunk_num % n_chunks)
+        y1 = chunk_size * (chunk_num // n_chunks)
         T_int_ultrablue[x1:x1+chunk_size,y1:y1+chunk_size] = np.exp(-tau_band_avgs_chunk[0])
         T_int_blue[x1:x1+chunk_size,y1:y1+chunk_size] = np.exp(-tau_band_avgs_chunk[1])
         T_int_center[x1:x1+chunk_size,y1:y1+chunk_size] = np.exp(-tau_band_avgs_chunk[2])
@@ -55,7 +55,7 @@ def transmission_integrated_z0_banded(z0_ss):
         T_int_ultrared[x1:x1+chunk_size,y1:y1+chunk_size] = np.exp(-tau_band_avgs_chunk[4])
     return z0, T_int_ultrablue, T_int_blue, T_int_center, T_int_red, T_int_ultrared
 
-def transmission_integrated_z0(z0_ss):
+def transmission_integrated_z0_unbanded(z0_ss):
     "Takes in a file created by calculate_tau.py, which includes z0, taus, transmission, Dvs"
     n_chunks = int(np.sqrt(len(z0_ss)))
     s0 = z0_ss[0]
